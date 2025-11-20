f05d347cbef89dc011ea98cf5d06ea24931f1240
eclark33 <eliseclark13@gmail.com>
2025-11-20 13:12:49 -0700

    Added data files

--- Walmart Time Series.R
+++ Walmart Time Series.R
@@ -1,3 +1,174 @@
+# libraries
+library(tidyverse)
+library(tidymodels)
+library(vroom)
+
+
+
+# read in data
+train_data <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/WalmartComp/train.csv")
+testData <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/WalmartComp/test.csv")
+
+# additional data sets
+stores <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/WalmartComp/stores.csv")
+features <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/WalmartComp/features.csv")
+
+
+############################# DATA PREPPING ###############################
+
+# create day of week variable
+train_data <- train_data %>%
+  mutate(date_doy = yday(Date))
+
+
+# replace markdown NAs with 0s
+features <- features %>% 
+  mutate(MarkDown1 = replace_na(MarkDown1, 0),
+         MarkDown2 = replace_na(MarkDown2, 0),
+         MarkDown3 = replace_na(MarkDown3, 0),
+         MarkDown4 = replace_na(MarkDown4, 0),
+         MarkDown5 = replace_na(MarkDown5, 0))
+
+# create TotalMarkdown variable (sum of markdowns)
+features <- features %>%
+  mutate(TotalMarkdown = rowSums(across(c(MarkDown1, MarkDown2, MarkDown3, MarkDown4, MarkDown5))))
+
+# create MarkdownFlag variable (1 if markdown 0 if not)
+features <- features %>%
+  mutate(MarkdownFlag = if_else(TotalMarkdown != 0, 1, 0))
+
+# impute Missing CPI and unemployment
+feature_recipe <- recipe(~., data=features) %>%
+  step_mutate(DecDate = decimal_date(Date)) %>%
+  step_impute_bag(CPI, Unemployment,
+                  impute_with = imp_vars(DecDate, Store))
+
+imputed_features <- juice(prep(feature_recipe))
+
+
+
+# join
+train_data <- train_data %>%
+  left_join(features, by = c("Store", "Date"))
+
+
+
+
+
+############################# EDA ###############################
+# look at stores & departments in test and training sets
+stores_test <- unique(testData$Store)
+stores_train <- unique(train_data$Store)
+
+stores_test
+stores_train
+
+# look at sample size for each store and department 
+sample_sizes <- train_data %>% group_by(Store, Dept) %>%
+  summarize(n = n())
+
+# look at holidays
+train_data %>%
+  filter(IsHoliday.x == TRUE) %>%    
+  mutate(Month = month(Date, label = TRUE)) %>%   
+  group_by(Month) %>%
+  summarise(n = n(), .groups = "drop") %>%
+  arrange(Month)
+
+
+# time series plot
+train_data %>% 
+  filter(Store == 1, Dept == 1) %>%
+  ggplot(aes(x = Date, y = Weekly_Sales)) +
+  geom_line() +
+  geom_smooth(se = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+################################# MODELING #######################################
+
+# recipe
+walmart_recipe <- recipe(Weekly_Sales ~ ., data = train_data) %>%
+                    step_range(date_doy, min = 0, max = pi) %>%
+                      step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy))
+                     
+
+
+
+
+
+# find all combinations 
+store_dept_pairs <- train_data %>%
+  distinct(Store, Dept)
+
+sampled_pairs <- store_dept_pairs %>%
+  sample_n(1)
+
+
+
+################ store 1 dept 80 ##############
+s1dept80 <- train_data %>%
+  filter(Dept == 80 & Store == 1)
+
+# model using random forest
+forest_mod <- rand_forest(mtry = tune(),
+                          min_n = tune(),
+                          trees = 500) %>% 
+  set_engine("ranger") %>% 
+  set_mode("regression")
+
+
+# forest recipe
+s1dept80_recipe <- recipe(Weekly_Sales ~ ., data = s1dept80) %>%
+  step_range(date_doy, min = 0, max = pi) %>%
+  step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy))
+
+
+# workflow  
+s1dept80_workflow <- workflow() %>%
+  add_recipe(s1dept80_recipe) %>%
+  add_model(forest_mod)
+
+# grid of values to tune 
+grid_of_tuning_params <- grid_regular(mtry(range = c(1,10)),
+                                      min_n(),
+                                      levels = 3) 
+
+# split data for cv & run it 
+folds <- vfold_cv(s1dept80, v = 5, repeats = 1)
+
+CV_results <- s1dept80_workflow %>%
+  tune_grid(resamples = folds,
+            grid = grid_of_tuning_params,
+            metrics = metric_set(mae), 
+            control = control_grid(verbose = TRUE))
+
+
+
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "mae")
+
+## finalize the workflow & fit 
+final_wf <- s1dept80_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = s1dept80) 
+
+
+show_best(CV_results, metric = "mae", n = 1)
+
+CV_results %>% 
+  collect_metrics() %>% 
+  filter(.metric == "mae") %>% 
+  semi_join(bestTune, by = c("mtry", "min_n"))
 
 
 
@@ -11,8 +182,178 @@
 
 
 
+##################### store 22 dept 35 ####################
+s22dept35 <- train_data %>%
+  filter(Dept == 22 & Store == 35)
+
+# model using random forest
+forest_mod <- rand_forest(mtry = tune(),
+                          min_n = tune(),
+                          trees = 500) %>% 
+  set_engine("ranger") %>% 
+  set_mode("regression")
+
+s22dept35 <- s22dept35 %>%
+  rename(IsHoliday = IsHoliday.x) %>%
+  select(-IsHoliday.y)
+
+
+# forest recipe
+s22dept35_recipe <- recipe(Weekly_Sales ~ ., data = s22dept35) %>%
+  step_mutate(Store = factor(Store),
+              Dept = factor(Dept),
+              IsHoliday = factor(IsHoliday)) %>%
+  step_range(date_doy, min = 0, max = pi) %>%
+  step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy)) %>%
+  step_normalize(all_numeric_predictors()) 
+
+
+# workflow  
+s22dept35_workflow <- workflow() %>%
+  add_recipe(s22dept35_recipe) %>%
+  add_model(forest_mod)
+
+# grid of values to tune 
+grid_of_tuning_params <- grid_regular(mtry(range = c(1,10)),
+                                      min_n(),
+                                      levels = 3) 
+
+# split data for cv & run it 
+folds <- vfold_cv(s22dept35, v = 5, repeats = 1)
+
+CV_results <- s22dept35_workflow %>%
+  tune_grid(resamples = folds,
+            grid = grid_of_tuning_params,
+            metrics = metric_set(mae), 
+            control = control_grid(verbose = TRUE))
+
+
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "mae")
+
+## finalize the workflow & fit 
+final_wf <- s22dept35_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = s22dept35) 
+
+
+show_best(CV_results, metric = "mae", n = 1)
 
 
 
+
+
+###################### store 15 dept 50 ###################
+s15dept50 <- train_data %>%
+  filter(Dept == 50 & Store == 15)
+
+
+# model using random forest
+forest_mod <- rand_forest(mtry = tune(),
+                          min_n = tune(),
+                          trees = 500) %>% 
+  set_engine("ranger") %>% 
+  set_mode("regression")
+
+s15dept50 <- s15dept50 %>%
+  rename(IsHoliday = IsHoliday.x) %>%
+  select(-IsHoliday.y)
+
+
+# forest recipe
+s15dept50_recipe <- recipe(Weekly_Sales ~ ., data = s15dept50) %>%
+  step_mutate(Store = factor(Store),
+              Dept = factor(Dept),
+              IsHoliday = factor(IsHoliday)) %>%
+  step_range(date_doy, min = 0, max = pi) %>%
+  step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy)) %>%
+  step_mutate(Holiday_Name = case_when(
+      Date %in% as.Date(c("2010-02-12","2011-02-11","2012-02-10","2013-02-08")) ~ "SuperBowl",
+      Date %in% as.Date(c("2010-09-10","2011-09-09","2012-09-07","2013-09-06")) ~ "LaborDay",
+      Date %in% as.Date(c("2010-11-26","2011-11-25","2012-11-23","2013-11-29")) ~ "Thanksgiving",
+      Date %in% as.Date(c("2010-12-31","2011-12-30","2012-12-28","2013-12-27")) ~ "Christmas",
+      TRUE ~ "None")) %>%
+  step_mutate(Holiday_Name = factor(Holiday_Name)) %>%
+  step_lag(Weekly_Sales, lag = c(1, 2, 3, 52)) %>%
+  step_impute_mean(starts_with("Weekly_Sales_lag"))
+
+
+
+# workflow  
+s15dept50_workflow <- workflow() %>%
+  add_recipe(s15dept50_recipe) %>%
+  add_model(forest_mod)
+
+# grid of values to tune 
+grid_of_tuning_params <- grid_regular(mtry(range = c(1,10)),
+                                      min_n(),
+                                      levels = 3) 
+
+# split data for cv & run it 
+folds <- vfold_cv(s15dept50, v = 5, repeats = 1)
+
+CV_results <- s15dept50_workflow %>%
+  tune_grid(resamples = folds,
+            grid = grid_of_tuning_params,
+            metrics = metric_set(mae), 
+            control = control_grid(verbose = TRUE))
+
+
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "mae")
+
+## finalize the workflow & fit 
+final_wf <- s15dept50_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = s15dept50) 
+
+
+show_best(CV_results, metric = "mae", n = 1)
+
+
+
+
+
+
+
+
+# 4322
+
+
+
+
+
+
+
+
+# have different time series for each store-dept combination 
+
+# ISSUES & HOW TO RESOLVE
+# missing values -> imputation (put it in yourself), make missing values 0s
+# feature engineer the heck out of the Date variable (month, week specific holiday, numerical date)
+# small sample size -> use simple model, predict 0s
+
+
+
+# dr heaton's code 
+### Impute Missing Markdowns
+features <- features %>%
+  mutate(across(starts_with("MarkDown"), ~ replace_na(., 0))) %>%
+  mutate(across(starts_with("MarkDown"), ~ pmax(., 0))) %>%
+  mutate(
+    MarkDown_Total = rowSums(across(starts_with("MarkDown")), na.rm = TRUE),
+    MarkDown_Flag = if_else(MarkDown_Total > 0, 1, 0),
+    MarkDown_Log   = log1p(MarkDown_Total)
+  ) %>%
+  select(-MarkDown1, -MarkDown2, -MarkDown3, -MarkDown4, -MarkDown5)
+
+## Impute Missing CPI and Unemployment
+feature_recipe <- recipe(~., data=features) %>%
+  step_mutate(DecDate = decimal_date(Date)) %>%
+  step_impute_bag(CPI, Unemployment,
+                  impute_with = imp_vars(DecDate, Store))
+imputed_features <- juice(prep(feature_recipe))
 
 
